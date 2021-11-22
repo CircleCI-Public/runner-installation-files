0b8ab9df74e3a0d6a193a0b79ed2b671bf87c4cc
Elaine <elaine@circleci.com>
2021-11-22 17:16:57 +0000

    first pass at updating download script to get download url from distributor

--- download-launch-agent.sh
+++ download-launch-agent.sh
@@ -9,13 +9,20 @@ sudo mkdir -p "${prefix}/workdir"
 # Downloading launch agent
 echo "Using CircleCI Launch Agent version ${agent_version}"
 echo "Downloading and verifying CircleCI Launch Agent Binary"
-base_url="https://circleci-binary-releases.s3.amazonaws.com/circleci-launch-agent"
-curl -sSL "${base_url}/${agent_version}/checksums.txt" -o checksums.txt
-file="$(grep "${platform}/circleci-launch-agent$" checksums.txt | cut -d ' ' -f 2 | sed 's/^.//')"
+
+split_platform=(${platform//// })
+echo ${split_platform[0]}
+echo ${split_platform[1]}
+runner_url="https://runner.circleci.com"
+download_response=$(curl -H "Accept: application/json" -H "Content-Type: application/json" --data "{\"os\":\"${split_platform[0]}\", \"arch\":\"${split_platform[1]}\"}" --request GET ${runner_url}/api/v2/runner/download)
+url=$(echo ${download_response} | jq -r .url)
+checksum=$(echo ${download_response} | jq -r .checksum)
+
+file=circleci-agent
 mkdir -p "${platform}"
 echo "Downloading CircleCI Launch Agent: ${file}"
-curl --compressed -L "${base_url}/${agent_version}/${file}" -o "${file}"
+curl --compressed -L "${url}" -o "${file}"
 
 # Verifying download
 echo "Verifying CircleCI Launch Agent download"
-grep "${file}$" checksums.txt | sha256sum --check && chmod +x "${file}"; sudo cp "${file}" "${prefix}/circleci-launch-agent" || echo "Invalid checksum for CircleCI Launch Agent, please try download again"
+echo "${checksum} ${file}" | sha256sum --check && chmod +x "${file}"; sudo cp "${file}" "${prefix}/circleci-launch-agent" || echo "Invalid checksum for CircleCI Launch Agent, please try download again"
