0bfa2cb5308d35f648d2df815223efce2df2c0e5
Chris Stephen <chris.stephen@circleci.com>
2021-11-23 10:18:39 -0400

    Address shellcheck errors

--- download-launch-agent.sh
+++ download-launch-agent.sh
@@ -5,25 +5,26 @@ set -euo pipefail
 # Set up runner directory
 prefix=/opt/circleci
 sudo mkdir -p "${prefix}/workdir"
-[[ -z ${runner_url+z} ]] && echo "\${runner_url} not defined setting default" && runner_url="https://runner.circleci.com" 
-[[ -z ${agent_version+z} ]] && agent_version="" 
+[[ -z ${runner_url+z} ]] && runner_url="https://runner.circleci.com"
+[[ -z ${agent_version+z} ]] && agent_version=""
+[[ -z ${platform+z} ]] && echo "platform not defined" && exit 1
 
 # Downloading launch agent
 echo "Using CircleCI Launch Agent version ${agent_version}"
 echo "Downloading and verifying CircleCI Launch Agent Binary"
 
-split_platform=(${platform//// })
-echo ${split_platform[0]}
+IFS="/" read -r -a split_platform <<<"${platform}"
 download_response=$(curl -H "Accept: application/json" -H "Content-Type: application/json" --data "{\"os\":\"${split_platform[0]}\", \"arch\":\"${split_platform[1]}\"}" --request GET ${runner_url}/api/v2/runner/download)
-url=$(echo ${download_response} | jq -r .url)
-checksum=$(echo ${download_response} | jq -r .checksum)
+url=$(echo "${download_response}" | jq -r .url)
+checksum=$(echo "${download_response}" | jq -r .checksum)
 
-split_url=(${url//// })
-file=${split_url[${#split_url[@]}-1]}
+IFS="/" read -r -a split_url <<<"${url}"
+file=${split_url[${#split_url[@]} - 1]}
 mkdir -p "${platform}"
 echo "Downloading CircleCI Launch Agent: ${file}"
 curl --compressed -L "${url}" -o "${file}"
 
 # Verifying download
 echo "Verifying CircleCI Launch Agent download"
-echo "${checksum} ${file}" | sha256sum --check && chmod +x "${file}"; sudo cp "${file}" "${prefix}/circleci-launch-agent" || echo "Invalid checksum for CircleCI Launch Agent, please try download again"
+echo "${checksum} ${file}" | sha256sum --check && chmod +x "${file}"
+sudo cp "${file}" "${prefix}/circleci-launch-agent" || echo "Invalid checksum for CircleCI Launch Agent, please try download again"
