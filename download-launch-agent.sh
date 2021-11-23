140cdd3a847a2705a775f6be23b78fd52863bffb
Elaine <elaine@circleci.com>
2021-11-23 11:20:45 +0000

    getting file name from end of url and small clean up

--- download-launch-agent.sh
+++ download-launch-agent.sh
@@ -5,6 +5,8 @@ set -euo pipefail
 # Set up runner directory
 prefix=/opt/circleci
 sudo mkdir -p "${prefix}/workdir"
+[[ -z ${runner_url+z} ]] && echo "\${runner_url} not defined setting default" && runner_url="https://runner.circleci.com" 
+[[ -z ${agent_version+z} ]] && agent_version="" 
 
 # Downloading launch agent
 echo "Using CircleCI Launch Agent version ${agent_version}"
@@ -12,13 +14,12 @@ echo "Downloading and verifying CircleCI Launch Agent Binary"
 
 split_platform=(${platform//// })
 echo ${split_platform[0]}
-echo ${split_platform[1]}
-runner_url="https://runner.circleci.com"
 download_response=$(curl -H "Accept: application/json" -H "Content-Type: application/json" --data "{\"os\":\"${split_platform[0]}\", \"arch\":\"${split_platform[1]}\"}" --request GET ${runner_url}/api/v2/runner/download)
 url=$(echo ${download_response} | jq -r .url)
 checksum=$(echo ${download_response} | jq -r .checksum)
 
-file=circleci-agent
+split_url=(${url//// })
+file=${split_url[${#split_url[@]}-1]}
 mkdir -p "${platform}"
 echo "Downloading CircleCI Launch Agent: ${file}"
 curl --compressed -L "${url}" -o "${file}"
