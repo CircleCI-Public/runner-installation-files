26b3c8402c1c402c18f912cf9cd9aa88d82325d1
Chris Stephen <chris.stephen@circleci.com>
2021-11-24 08:13:46 -0400

    Rename variables

--- download-launch-agent.sh
+++ download-launch-agent.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 # Set up runner directory
 prefix=/opt/circleci
 sudo mkdir -p "${prefix}/workdir"
-[[ -z ${runner_url+z} ]] && runner_url="https://runner.circleci.com"
+[[ -z ${api_url+z} ]] && api_url="https://runner.circleci.com"
 [[ -z ${agent_version+z} ]] && agent_version=""
 [[ -z ${platform+z} ]] && echo "platform not defined" && exit 1
 [[ -z ${CIRCLECI_RUNNER_TOKEN+z} ]] && echo "CIRCLECI_RUNNER_TOKEN not defined" && exit 1
@@ -15,15 +15,15 @@ echo "Using CircleCI Launch Agent version ${agent_version}"
 echo "Downloading and verifying CircleCI Launch Agent Binary"
 
 IFS="/" read -r -a split_platform <<<"${platform}"
-download_response=$(curl -H "Authorization: Bearer ${CIRCLECI_RUNNER_TOKEN}" -H "Accept: application/json" -H "Content-Type: application/json" --data "{\"os\":\"${split_platform[0]}\", \"arch\":\"${split_platform[1]}\", \"version\":\"${agent_version}\"}" --request GET ${runner_url}/api/v2/launch-agent/download)
-url=$(echo "${download_response}" | jq -r .url)
+download_response=$(curl -H "Authorization: Bearer ${CIRCLECI_RUNNER_TOKEN}" -H "Accept: application/json" -H "Content-Type: application/json" --data "{\"os\":\"${split_platform[0]}\", \"arch\":\"${split_platform[1]}\", \"version\":\"${agent_version}\"}" --request GET ${api_url}/api/v2/launch-agent/download)
+download_url=$(echo "${download_response}" | jq -r .url)
 checksum=$(echo "${download_response}" | jq -r .checksum)
 
-IFS="/" read -r -a split_url <<<"${url}"
-file=${split_url[${#split_url[@]} - 1]}
+IFS="/" read -r -a split_download_url <<<"${download_url}"
+file=${split_download_url[${#split_download_url[@]} - 1]}
 mkdir -p "${platform}"
 echo "Downloading CircleCI Launch Agent: ${file}"
-curl --compressed -L "${url}" -o "${file}"
+curl --compressed -L "${download_url}" -o "${file}"
 
 # Verifying download
 echo "Verifying CircleCI Launch Agent download"
